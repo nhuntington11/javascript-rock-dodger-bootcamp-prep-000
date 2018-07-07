f6077d555068a3e24928b0a13b304f6048ab4c54
Nick H <nhuntington11@gmail.com>
2018-07-07 10:43:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -130,9 +130,8 @@ function moveDodgerRight() {
   var leftValueInteger = parseInt(leftValue, 10)
   
   function moveRight() {
-    DODGER.style.left = `${leftValueInteger + 4}px`
-    
     if (leftValueInteger < 360) {
+      DODGER.style.left = `${leftValueInteger + 4}px`
       window.requestAnimationFrame(moveRight)
     }
   }
